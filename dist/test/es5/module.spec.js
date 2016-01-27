c96f9f27989cffad67231bd74bc7cbb3ecded5c4
sylvaindethier <dethiersylvain@gmail.com>
2016-01-27 19:09:37 +0100

    add dist tests

---
+++ dist/test/es5/module.spec.js
@@ -0,0 +1,17 @@
+var expect = require('expect');
+var _module = require('../../react-optsprops');
+
+var opts = _module.default;
+describe('module', () => {
+  it('should expose a default function (opts)', () => {
+    expect(opts).toBeA('function');
+  });
+
+  it('should expose a "PropTypesOpts" property', () => {
+    expect(_module.PropTypesOpts).toNotBe(undefined);
+  });
+
+  it('should expose a "ComponentOpts" property', () => {
+    expect(_module.ComponentOpts).toNotBe(undefined);
+  });
+});
---
+++ dist/test/es6/module.spec.js
@@ -0,0 +1,16 @@
+import expect from 'expect';
+import opts, { PropTypesOpts, ComponentOpts } from '../../react-optsprops';
+
+describe('module', () => {
+  it('should expose a default function (opts)', () => {
+    expect(opts).toBeA('function');
+  });
+
+  it('should expose a "PropTypesOpts" property', () => {
+    expect(PropTypesOpts).toNotBe(undefined);
+  });
+
+  it('should expose a "ComponentOpts" property', () => {
+    expect(ComponentOpts).toNotBe(undefined);
+  });
+});
