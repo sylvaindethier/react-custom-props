145421b96485025f3cd4a386d1f016224cbd0c5c
sylvaindethier <dethiersylvain@gmail.com>
2016-01-27 20:17:05 +0100

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-package="package.json"
-
 ## prevent runing script outside the repo root
 if ! [ -e scripts/release.sh ]; then
   echo >&2 "Please run scripts/release.sh from the repo root"
@@ -12,19 +10,19 @@ fi
 npm run build
 
 ## get the current version from package
-current_version=$(node -p "require('./${package}').version")
+current_version=$(node -p "require('./package').version")
 ## read the version to update
 printf "Update type (current version is $current_version)? "
 read update_type
 
 ## update package version
-npm version $update_type
+version=$(npm version $update_type)
 
 ## process git
-git commit --allow-empty -am "Update $update_type"
-git tag $update_type
+git commit --allow-empty -am "Version $version"
+git tag $version
 git push origin master
-git push origin $update_type
+git push origin $version
 
 ## publish to npm
 npm publish
