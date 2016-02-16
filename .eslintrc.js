dc7761c60e94addc96901dbd4f9e3212838a0df1
sylvaindethier <dethiersylvain@gmail.com>
2016-02-16 15:11:34 +0100

    rewrite dot files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,31 @@
-/* eslint no-inline-comments:0, comma-dangle:0 */
+/* eslint max-len:0 no-inline-comments:0 */
 module.exports = {
+  root: true,
+
   // parser: 'babel-eslint',
 
   env: {
     // browser: true, // browser global variables.
     node: true, // Node.js global variables and Node.js scoping.
     // commonjs: true, // CommonJS global variables and CommonJS scoping (use this for browser-only code that uses Browserify/WebPack).
-    // worker: false, // web workers global variables.
-    // amd: false, // defines require() and define() as global variables as per the amd spec.
-    mocha: true, // adds all of the Mocha testing global variables.
-    // jasmine: false, // adds all of the Jasmine testing global variables for version 1.3 and 2.0.
-    // jest: false, // Jest global variables.
-    // phantomjs: false, // PhantomJS global variables.
-    // protractor: false, // Protractor global variables.
-    // qunit: false, // QUnit global variables.
-    // jquery: false, // jQuery global variables.
-    // prototypejs: false, // Prototype.js global variables.
-    // shelljs: false, // ShellJS global variables.
-    // meteor: false, // Meteor global variables.
-    // mongo: false, // MongoDB global variables.
-    // applescript: false, // AppleScript global variables.
-    // nashorn: false, // Java 8 Nashorn global variables.
-    // serviceworker: false, // Service Worker global variables.
-    // embertest: false, // Ember test helper globals.
-    // webextensions: false, // WebExtensions globals.
+    // worker: true, // web workers global variables.
+    // amd: true, // defines require() and define() as global variables as per the amd spec.
+    // mocha: true, // adds all of the Mocha testing global variables.
+    // jasmine: true, // adds all of the Jasmine testing global variables for version 1.3 and 2.0.
+    // jest: true, // Jest global variables.
+    // phantomjs: true, // PhantomJS global variables.
+    // protractor: true, // Protractor global variables.
+    // qunit: true, // QUnit global variables.
+    // jquery: true, // jQuery global variables.
+    // prototypejs: true, // Prototype.js global variables.
+    // shelljs: true, // ShellJS global variables.
+    // meteor: true, // Meteor global variables.
+    // mongo: true, // MongoDB global variables.
+    // applescript: true, // AppleScript global variables.
+    // nashorn: true, // Java 8 Nashorn global variables.
+    // serviceworker: true, // Service Worker global variables.
+    // embertest: true, // Ember test helper globals.
+    // webextensions: true, // WebExtensions globals.
     // es6: true, // enable all ECMAScript 6 features except for modules.
   },
 
@@ -58,9 +60,13 @@ module.exports = {
 
   extends: [
     'eslint:recommended',
-    'google'
+    'google',
   ],
 
   rules: {
-  }
+    'brace-style': [2, '1tbs', { allowSingleLine: true }],
+    'comma-dangle': [2, 'always-multiline'],
+    'no-multiple-empty-lines': [2, { max: 2, maxEOF: 1 }],
+    'object-curly-spacing': [2, 'always'],
+  },
 };
