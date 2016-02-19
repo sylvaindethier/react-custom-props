6042c99f96a0218171f4261a70dd373a49eef174
sylvaindethier <dethiersylvain@gmail.com>
2016-02-19 21:43:27 +0100

    Update 1.0.0-alpha.0 from 0.3.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,20 +11,26 @@ npm run build
 
 ## get the current version from package
 current_version=$(node -p "require('./package').version")
-## read the version to update
-printf "Update type (current version is $current_version)? "
+
+## read the version update type
+echo "Current version is '$current_version'"
+echo "Update type: <version> | (pre)major | (pre)minor | (pre)patch | prerelease | from-git"
+printf "Which update type is ? "
 read update_type
 
-## commit unstaged changes
+## add and commit unstaged changes
 git add --all
 git commit --allow-empty -am "Update $update_type from $current_version"
-## update package version
-version=$(npm version $update_type)
 
-## push to origin
-#git tag $version
-git push origin master
-git push origin $version
+## update package version (disable git-tag-version, will be done after)
+version=$(npm --no-git-tag-version version $update_type)
+echo "New version is '$version'"
+
+## create tag version
+git tag -a "$version" -m "version $version"
+
+## push to origin with tag
+git push --follow-tag origin master
 
 ## publish to npm
 npm publish
