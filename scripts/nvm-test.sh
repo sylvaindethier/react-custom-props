c279f10989fdc26d7a859b1225c3532cdc7f6091
sylvaindethier <dethiersylvain@gmail.com>
2016-02-22 04:05:27 +0100

    add nvm test

--- scripts/nvm-test.sh
+++ scripts/nvm-test.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
-# TODO: should be read from .travis.yml
+# TODO: should be read from .travis.yml & package.json
 declare -a node_js=('node' '5' '4' '0.12' '0.10');
 peerDependencies='react'
+
 
 # prevent runing script outside the repo root
 if ! [ -e scripts/release.sh ]; then
@@ -10,31 +11,30 @@ if ! [ -e scripts/release.sh ]; then
   exit 1
 fi
 
-# for each version of Node we will:
-#   1. use the corresponding Node version
-#   2. remove node_modules
-#   3. install the project
-#   4. install the peerDependencies
-#   5. run the test
+# Load nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
+# Test if 'nvm --version' exit with error (!= 0)
+nvm --version &> /dev/null
+if [ $? -ne 0 ]; then
+  echo >&2 "Please install nvm from https://github.com/creationix/nvm#install-script"
+  exit 1
+fi
+
+# for each version of Node we will:
 test_node () {
   local version="$1"
 
-  echo "test for node $version"
-  nvm use "$version"
-  rm -rf node_modules
-  npm install
-  npm install "$peerDependencies"
+  # use the node $version
+  nvm use $version
+
+  # run the test
   npm test
 }
 
-# get the current node version
-current="$(nvm current)"
-
 # run the tests for each node versions
 for node_version in "${node_js[@]}"; do
-    test_node "$node_version"
+  echo "Begin test for node version '$node_version'"
+  test_node $node_version
+  echo ""
 done
-
-# restore the node versions
-nvm use "$current"
